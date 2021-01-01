fd98d4bde2cb4e7c6c4da26987ea0061b5d7c6d0
Deepak Choudhari <deepak.choudhari@outlook.com>
2020-12-31 19:21:33 -0500

    Initial commit

---
+++ src/routes/Movies.ts
@@ -0,0 +1,11 @@
+import { IRequest } from '@shared/constants';
+import Router, { IRouter } from 'express';
+
+const router: IRouter = Router();
+
+router.get("/", (req: IRequest) => {
+
+});
+
+export default router;
+
---
+++ src/routes/index.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+import MovieRouter from './Movies';
+
+// Init router and path
+const router = Router();
+
+// Add sub-routes
+router.use("movies", MovieRouter);
+
+// Export the base-router
+export default router;

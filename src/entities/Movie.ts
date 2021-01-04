426cd694ad746b054ba761f6245096e3444fc112
Deepak Choudhari <deepak.choudhari@outlook.com>
2021-01-04 06:47:10 -0500

    Add Movie interfaces

---
+++ src/entities/Movie.ts
@@ -0,0 +1,17 @@
+export interface IMovie {
+    year: number;
+    title: string;
+    info: IMovieInfo;
+}
+
+export interface IMovieInfo {
+    directors: string[];
+    release_date: string;
+    genres: string[];
+    image_url: string;
+    plot: string;
+    rank: number;
+    actors: string[];
+    running_time_secs?: number;
+    rating?: number;
+}

de0659def76aa4b5107dfc65626268694453d143
Deepak Choudhari <deepak.choudhari@outlook.com>
2021-01-07 14:45:07 -0500

    Add return types for functions

--- src/shared/functions.ts
+++ src/shared/functions.ts
@@ -1,11 +1,11 @@
 import logger from './logger';
 
-export const pErr = (err: Error) => {
+export const pErr = (err: Error): void => {
     if (err) {
         logger.err(err);
     }
 };
 
-export const getRandomInt = () => {
+export const getRandomInt = (): number => {
     return Math.floor(Math.random() * 1_000_000_000_000);
 };

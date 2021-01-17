b7cbaaffdc33e201c0256a82692c5cf199d31988
Deepak Choudhari <deepak.choudhari@outlook.com>
2021-01-17 17:08:29 -0500

    Refactor route handles into separate class

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,18 @@
+import { IMovieDbContext, MovieDbContext } from '@dataaccess/movieDbContext';
+import logger from '@shared/logger';
 import { Router } from 'express';
-import MovieRouter from './movies';
+import { MovieRoutes } from './movies';
+import { createMovieRequestSchema, validatorFn } from './requestValidators';
 
 // Init router and path
 const router = Router();
+const movieRepository: IMovieDbContext = new MovieDbContext(logger);
+const movieRoutes = new MovieRoutes(router, movieRepository, logger);
 
 // Add sub-routes
-router.use(MovieRouter);
+router.get('/movies', movieRoutes.getAllMovies);
+router.post('/movies', validatorFn({ body: createMovieRequestSchema }), movieRoutes.postMovie);
+router.get('/movie/:year', movieRoutes.getMovieByYear);
 
 // Export the base-router
 export default router;
--- src/routes/movies.ts
+++ src/routes/movies.ts
@@ -1,15 +1,54 @@
 import { StatusCodes } from 'http-status-codes';
-import { Request, Response, Router } from 'express';
+import { IRouter, Request, Response } from 'express';
+import { IMovieDbContext } from '@dataaccess/movieDbContext';
+import logger from '@shared/logger';
+import { IMovie } from '@entities/movie';
+import Logger from 'jet-logger';
 
-const router: Router = Router();
+/**
+ * Class definition for all request handlers
+ */
+export class MovieRoutes {
+    private readonly router: IRouter;
+    private readonly movieRepository: IMovieDbContext;
+    private readonly logger: Logger;
 
-router.get('/movies', (req: Request, res: Response) => {
-    return res.status(StatusCodes.OK).json({ data: `${new Date()}` });
-});
+    constructor(router: IRouter, movieRepository: IMovieDbContext, logger: Logger) {
+        this.router = router;
+        this.movieRepository = movieRepository;
+        this.logger = logger;
+    }
 
-router.get('/movies/:year', (req: Request, res: Response) => {
-    const { year } = req.params;
-    return res.status(StatusCodes.OK).json({ year, data: `${new Date()}` });
-});
+    /**
+     * POST /api/v1/movie
+     * @param req Request object
+     * @param res Response object
+     */
+    async postMovie(req: Request, res: Response): Promise<Response> {
+        logger.info(`Creating movie: ${JSON.stringify(req.body)}`);
+        const movie = <IMovie>req.body;
+        const result = await this.movieRepository.create(movie);
+        
+        return result? res.status(StatusCodes.CREATED).json() : 
+            res.status(StatusCodes.INTERNAL_SERVER_ERROR).json();            
+    }
 
-export default router;
+    /**
+     * GET /api/v1/movies
+     * @param req Request object
+     * @param res Response object
+     */
+    async getAllMovies(req: Request, res: Response): Promise<Response> {
+        return res.status(StatusCodes.OK).json({ data: `${new Date()}` });
+    }
+
+    /**
+     * GET /api/v1/movie/2001
+     * @param req Request object
+     * @param res Response object
+     */
+    async getMovieByYear(req: Request, res: Response): Promise<Response> {
+        const { year } = req.params;
+        return res.status(StatusCodes.OK).json({ year, data: `${new Date()}` });    
+    }
+}
